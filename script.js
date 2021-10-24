9522ae75f9b5b57fdbe1d025a691a0654eaf185b
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-25 01:55:41 +0300

    Добавляем новые элементы с текстом из инпута

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-let button = document.querySelector('#btn')
-let text = document.querySelector('#text')
-let square = document.querySelector('#square')
-let circle = document.querySelector('#circle')
-let range = document.querySelector('#range')
-let span = document.querySelector('#range-span')
-
+let list = document.querySelector("ul");
+let allList = document.querySelectorAll("li")
+let btn = document.querySelector("button")
 
 let logger = function () {
-    square.style.background = text.value;
+
+    let newListItem = document.createElement("li")
+    let text = document.querySelector("#cd");
+    newListItem.innerHTML = text.value
+    list.append(newListItem)
 }
 
-button.addEventListener('click', logger);
+
+btn.addEventListener("click", logger)
 
 
-range.addEventListener('input', function () {
-    span.textContent = range.value;
-    circle.setAttribute("style", `width: ${range.value}%; height: ${range.value}%`)
-
-    //circle.style.width = range.value + "%"
-    //circle.style.height = range.value + "%"
-
-})
 
 
-document.querySelector("#e_btn").setAttribute("style", "display: none")
+
