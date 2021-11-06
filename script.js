c5621e60798bf281b1a5a79fea761effb0896538
vsheva <valera.sheva@gmail.com>
2021-11-07 01:03:59 +0200

    Completed task

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
-let list = document.querySelector("ul");
-let allList = document.querySelectorAll("li")
-let btn = document.querySelector("button")
+let button = document.querySelector('#btn')
+let text = document.querySelector('#text')
+let square = document.querySelector('#square')
+let circle = document.querySelector('#circle')
+let range = document.querySelector('#range')
+let span = document.querySelector('#range-span')
+
 
 let logger = function () {
-
-    let newListItem = document.createElement("li")
-    let text = document.querySelector("#cd");
-    newListItem.innerHTML = text.value
-    list.append(newListItem)
+    square.style.background = text.value;
 }
 
-
-btn.addEventListener("click", logger)
-
+button.addEventListener('click', logger);
 
 
+range.addEventListener('input', function () {
+    span.textContent = range.value;
+    circle.setAttribute("style", `width: ${range.value}%; height: ${range.value}%`)
+
+    //circle.style.width = range.value + "%"
+    //circle.style.height = range.value + "%"
+
+})
 
 
+document.querySelector("#e_btn").setAttribute("style", "display: none")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let list = document.querySelector("ul");
+// let allList = document.querySelectorAll("li")
+// let btn = document.querySelector("button")
+//
+// let logger = function () {
+//
+//     let newListItem = document.createElement("li")
+//     let text = document.querySelector("#cd");
+//     newListItem.innerHTML = text.value
+//     list.append(newListItem)
+// }
+//
+//
+// btn.addEventListener("click", logger)
+//
+//
